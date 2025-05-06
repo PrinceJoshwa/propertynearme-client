dc0e61c1a6becbc3b2ab8d51f6cbbbe814e9bc07
PrinceJoshwa <sprincejoshwa85@gmail.com>
2025-05-06 12:47:08 +0530

    first commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api"
+const API_URL = import.meta.env.VITE_API_URL || "https://propertynearme-server.vercel.app/api"
 
 
 export const googleAuth = async (token) => {
